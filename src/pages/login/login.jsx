6aaff97cc7e056bfbfef602e4db7229a4cb7faea
良升 <975135062@qq.com>
2021-05-05 21:27:44 +0800

    Input组件

--- src/pages/login/login.jsx
+++ src/pages/login/login.jsx
@@ -1,62 +1,92 @@
-import React, { Component } from "react";
-import { Form, Input, Button } from "antd";
-import { UserOutlined, LockOutlined } from "@ant-design/icons";
+import React from "react";
+import { Form, Input, Button, Checkbox } from 'antd';
+import { UserOutlined, LockOutlined } from '@ant-design/icons';
 import "./login.less";
 // 登录的路由组件
-class Login extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {};
-  }
-  render() {
-    return (
-      <div className="login">
-        <header className="login-header">
-          <img src="https://static.ygyg.cn/static/brand/sso-web/yunadmin/logo.png" />
-          <h1>谷粒-后台管理系统</h1>
-        </header>
-        <section className="login-content">
-          <h2>用户登录</h2>
-          <Form name="horizontal_login" layout="inline">
-            <Form.Item
-              name="username"
-              rules={[
-                {
-                  required: true,
-                  message: "Please input your username!",
-                },
-              ]}
-            >
-              <Input
-                prefix={<UserOutlined className="site-form-item-icon" />}
-                placeholder="Username"
-              />
+const NormalLoginForm = () => {
+  const onFinish = (values) => {
+    console.log('Received values of form: ', values);
+  };
+  return (
+    <div className="login">
+      <header className="login-header">
+        <img src="https://static.ygyg.cn/static/brand/sso-web/yunadmin/logo.png" />
+        <h1>谷粒-后台管理系统</h1>
+      </header>
+      <section className="login-content">
+        <h2>用户登录</h2>
+        <Form
+          name="normal_login"
+          className="login-form"
+          initialValues={{
+            remember: true,
+          }}
+          onFinish={onFinish}
+        >
+          <Form.Item
+            name="username"
+            rules={[
+              {
+                required: true,
+                whitespace:true,
+                message: '用户名不能为空！',
+              },
+            ]}
+          >
+            <Input
+              prefix={<UserOutlined className="site-form-item-icon" />}
+              placeholder="Username"
+              style={{ color: 'rgba(0, 0, 0, .25)' }}
+            />
+          </Form.Item>
+          <Form.Item
+            name="password"
+            rules={[
+              {
+                required: true,
+                whitespace:true,
+                message: '密码不能为空！',
+              },
+              {
+                min: 4,
+                message: '密码不能低于4位！',
+              },
+              {
+                max: 12,
+                message: '密码不能超过12位！',
+              },
+              {
+                pattern: /^[a-zA-Z0-9_]+$/,
+                message: '密码只能由字母、数字或下划线组成！',
+              },
+            ]}
+          >
+            <Input
+              prefix={<LockOutlined className="site-form-item-icon" />}
+              style={{ color: 'rgba(0, 0, 0, .25)' }}
+              type="password"
+              placeholder="Password"
+            />
+          </Form.Item>
+          <Form.Item>
+            <Form.Item name="remember" valuePropName="checked" noStyle>
+              <Checkbox>Remember me</Checkbox>
             </Form.Item>
-            <Form.Item
-              name="password"
-              rules={[
-                {
-                  required: true,
-                  message: "Please input your password!",
-                },
-              ]}
-            >
-              <Input
-                prefix={<LockOutlined className="site-form-item-icon" />}
-                type="password"
-                placeholder="Password"
-              />
-            </Form.Item>
-            <Form.Item>
-              <Button type="primary" htmlType="submit" className='login-form-button'>
-                登录
-              </Button>
-            </Form.Item>
-          </Form>
-        </section>
-      </div>
-    );
-  }
-}
 
-export default Login;
+            <a className="login-form-forgot" href="">
+              Forgot password
+        </a>
+          </Form.Item>
+
+          <Form.Item>
+            <Button type="primary" htmlType="submit" className="login-form-button">
+              登录
+              </Button>
+              Or <a href="">register now!</a>
+          </Form.Item>
+        </Form>
+      </section>
+    </div>
+  );
+}
+export default NormalLoginForm;
